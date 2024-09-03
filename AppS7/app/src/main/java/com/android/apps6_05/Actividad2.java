40fc93ff19d22ef76a6cc96e9085b5b70db50623
jesusmonges <galeanojesus182@gmail.com>
2024-09-02 21:52:52 -0400

    Tarea 3

---
+++ AppS7/app/src/main/java/com/android/apps6_05/Actividad2.java
@@ -0,0 +1,15 @@
+package com.android.apps6_05;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Actividad2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad2);
+
+        // Puedes agregar aquí cualquier inicialización adicional que necesites
+    }
+}
